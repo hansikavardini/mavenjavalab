06583545407c90f683c37672c99772e70cc30375
hansikavardini <vardinihansika@gmail.com>
2025-01-02 15:42:30 +0530

    Update App.java

--- src/main/java/SE_lab/mavenjava/App.java
+++ src/main/java/SE_lab/mavenjava/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "this is hansika rollno:22bd1a6658" );
+        System.out.println( "welcome to SE lab" );
     }
 }
