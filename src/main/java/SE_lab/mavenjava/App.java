5de4d1fe4517a03ff4aa70dfbef75570b351424a
angelena <angelena.shiny@gmail.com>
2025-01-02 14:56:24 +0530

    commit1

---
+++ src/main/java/SE_lab/mavenjava/App.java
@@ -0,0 +1,14 @@
+package SE_lab.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "this is hansika rollno:22bd1a6658" );
+    }
+}
